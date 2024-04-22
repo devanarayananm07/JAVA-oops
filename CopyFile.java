0cd67273c8b6660d31c4385be9c3d24e7f58f923
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-04-22 01:47:04 -0700

    Update CopyFile.java

--- CopyFile.java
+++ CopyFile.java
@@ -19,3 +19,24 @@ class CopyFile {
         fout.close();
     }
 }
+
+                   output
+---------------------------------------
+C:\Users\mlm\.jdks\openjdk-21.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\lib\idea_rt.jar=62877:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\mlm\IdeaProjects\j2\out\production\j2 CopyFile
+Provide source file name: source.txt
+
+
+source.txt
+---------------
+Java is a popular programming language, created in 1995.
+It is owned by Oracle, and more than 3 billion devices run Java.
+It is used for: Mobile applications (specially Android apps) Desktop applications
+
+Provide destination file name: destination.txt
+Copy finish...
+
+destination.txt
+-----------------------
+Java is a popular programming language, created in 1995.
+It is owned by Oracle, and more than 3 billion devices run Java.
+It is used for: Mobile applications (specially Android apps) Desktop applications
